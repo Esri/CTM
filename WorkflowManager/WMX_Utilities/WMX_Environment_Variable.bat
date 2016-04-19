ac757cd2e02416f556e954112a900d9dd2b4a410
Joseph Sheffield <jsheffield@esri.com>
2016-04-19 09:11:10 -0600

    Added the .bat file to set the WMX Path variable for the python script steps

---
+++ WorkflowManager/WMX_Utilities/WMX_Environment_Variable.bat
@@ -0,0 +1,4 @@
+REM  This .bat files sets the WMX_PATH variable for the CTM Workflow Manager (WMX) Workflows
+REM  This file needs to be run on each machine to point the WMX Workflow Script steps to the correct tool location.
+
+setx WMX_PATH "%CD%" /m
