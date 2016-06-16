5a8678491b202c4cd472b7aa7ec4611d5af0c6fa
Joseph Sheffield <jsheffield@esri.com>
2016-06-15 19:18:33 -0600

    updated the environment variable file for the WMX Path environment variable

--- WorkflowManager/WMX_Environment_Variable.bat
+++ WorkflowManager/WMX_Environment_Variable.bat
@@ -1,5 +1,8 @@
 REM  This .bat files sets the WMX_PATH variable for the CTM Workflow Manager (WMX) Workflows
 REM  This file needs to be run on each machine to point the WMX Workflow Script steps to the correct tool location.
 
-cd..
-setx WMX_PATH "%CD%" /m
+@echo off&setlocal
+for %%i in ("%~dp0..") do set "folder=%%~fi"
+echo %folder%
+
+setx WMX_PATH %folder% -m
